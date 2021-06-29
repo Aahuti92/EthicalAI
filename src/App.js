e4b642e53dd2d167961f9e137a8ba6926cb59286
AniketKumarRoy1 <roykumaraniket@gmail.com>
2021-06-30 01:23:50 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import NavbarComponent from './components/NavbarComponent/NavbarComponent';
 import AppBody from './components/AppBody/AppBody';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
-    <div>
-      <NavbarComponent />
-      <AppBody />
-    </div>
+    <Router>
+      <div class="d">
+        <NavbarComponent />
+        <Route path="/" exact component={AppBody} />
+      </div>
+    </Router>
   );
 }
 
